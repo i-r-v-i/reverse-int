c7ea697db0a74e275b6cf81f9f5c5319fc9038ae
Irina Vinitskaia <kreneva.i@yandex.ru>
2025-07-23 12:26:30 +0300

    feat: done-2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function reverse(n) {
-  // if (n < 0) {
-  //   n *= -1;
-  // }
+  if (n < 0) {
+    n *= -1;
+  }
   const num = `${n}`;
   const str = num.split('').reverse().join('');
   return Number(str);
